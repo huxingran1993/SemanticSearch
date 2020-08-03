2d525881f08ee2607ef37d542831b8340296c183
huxingran1993 <57091978+huxingran1993@users.noreply.github.com>
2020-08-03 13:53:03 +0200

    initialization

---
+++ src/main/java/com/interview/cognizone/CognizoneApplication.java
@@ -0,0 +1,13 @@
+package com.interview.cognizone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognizoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognizoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interview/cognizone/CognizoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.cognizone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognizoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
